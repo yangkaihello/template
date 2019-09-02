61846a89afdc2a1705db7a732c334d2e88a1617b
yangkaihello <444655291@qq.com>
2019-09-02 16:54:47 +0800

    1

--- heimiao-mobile/views/site/login-select.php
+++ heimiao-mobile/views/site/login-select.php
@@ -21,11 +21,11 @@ $this->title = "选择登陆";
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0,minimum=1.0,user-scalable=no">
 
-    <script src="/js/rem.js"></script>
-    <script src='/js/jquery-1.10.1.min.js'></script>
-    <script src='/js/rank.js'></script>
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/log.css">
+    <script src="/static/js/rem.js"></script>
+    <script src='/static/js/jquery-1.10.1.min.js'></script>
+    <script src='/static/js/rank.js'></script>
+    <link rel="stylesheet" href="/static/css/reset.css">
+    <link rel="stylesheet" href="/static/css/log.css">
 
     <title><?= $this->title ?></title>
 </head>
